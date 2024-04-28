749881103a946ead5a1eba579ecb4180c0e617aa
Maryline Renaud <r.maryline90@gmail.com>
2024-04-28 15:13:49 +0200

    my super Layout

---
+++ pres_html/layouts/mlayout.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps({
+  image: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div relative h-full w-full p-4>
+    <div flex="~ justify-end">
+      <img
+        v-motion
+        :initial="{ x: 100, opacity: 0 }"
+        :enter="{ x: 0, opacity: 0.5 }"
+        :leave="{ x: -100, opacity: 0 }"
+        :src="image"
+        alt="html Logo"
+      />
+    </div>
+    <slot />
+  </div>
+</template>
--- pres_html/slides.md
+++ pres_html/slides.md
@@ -1,4 +1,13 @@
 ---
 title: "Slides"
 src: "slides/slide01.md"
+layout: "defaultLayout"
 ---
+
+---
+title: "Slides"
+src: "slides/slide02.md"
+layout: "mlayout"
+image: "/html.svg"
+---
+
